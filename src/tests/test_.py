8247664bed991cdea6be181769e2088d73f7782f
dzaky.fattan <70198829+DzakyFattan@users.noreply.github.com>
2022-04-18 10:15:44 +0700

    fitur database ditambahkan

---
+++ src/tests/test_.py
@@ -0,0 +1,22 @@
+"""test for main.py"""
+
+import sys
+import os
+
+current = os.path.dirname(os.path.realpath(__file__))
+
+parent = os.path.dirname(current)
+
+sys.path.append(parent)
+
+from dbhandler import create_connection
+from main import main
+
+def test_connection():
+    """test for connection"""
+    assert create_connection() is not None
+
+def test_main(monkeypatch):
+    """test main"""
+    monkeypatch.setattr('builtins.input', lambda _: 13)
+    assert main() is not None
