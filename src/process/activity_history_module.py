75ebdc4f0648116ae0144778d3534c76babf3129
IMYELI <13520091@std.stei.itb.ac.id>
2022-04-20 13:29:19 +0700

    fix get_previous_day_sum and pylint

--- src/process/activity_history_module.py
+++ src/process/activity_history_module.py
@@ -1,14 +1,16 @@
+'''Module to handle activity record'''
 import enum
-from dbhandler import create_connection
 import random
 import datetime
+from dbhandler import create_connection
 
 mydb = create_connection()
 
 mycursor = mydb.cursor()
 
 def get_logged_user():
-    cmd = f"SELECT username FROM logged_user LIMIT 1"
+    ''' return logged user '''
+    cmd = "SELECT username FROM logged_user LIMIT 1"
     mycursor.execute(cmd)
     return mycursor.fetchone()[0]
 
@@ -18,20 +20,20 @@ class Category(enum.Enum):
     Kendaraan = 1
     Elektronik = 2
 
-'''Input data rekaman jejak karbon'''
 def input_data():
+    '''Input data rekaman jejak karbon'''
     nama_aktivitas = str(input('Nama Aktivitas: '))
     kategori = str(input('Jenis Aktivitas (Kendaraan / Elektronik): '))
     jumlah = int(input('Jumlah dalam KWh atau dalam Liter bensin: '))
     waktu = str(input('Waktu dalam format \"YYYY-MM-DD HH:MM:SS\": '))
     if waktu == '':
-        ct = datetime.datetime.now()
-        year = ct.date().year
-        month = ct.date().month
-        day = ct.date().day
-        hour = ct.hour
-        minute = ct.minute
-        second = ct.second
+        now = datetime.datetime.now()
+        year = now.date().year
+        month = now.date().month
+        day = now.date().day
+        hour = now.hour
+        minute = now.minute
+        second = now.second
         waktu = f"{year}-{month}-{day} {hour}:{minute}:{second}"
     if kategori == 'Kendaraan':
         data = (current_username, nama_aktivitas, Category.Kendaraan, jumlah, None, waktu)
@@ -39,8 +41,8 @@ def input_data():
         data = (current_username, nama_aktivitas, Category.Elektronik, None, jumlah, waktu)
     return data
 
-''' generate random timestamp '''
 def generate_timestamp():
+    ''' generate random timestamp '''
     year = random.randint(2021, 2022)
     month = random.randint(1, 12)
     day = random.randint(1, 28)
@@ -50,10 +52,11 @@ def generate_timestamp():
     time = f"{year}-{month}-{day} {hour}:{minute}:{second}"
     return time
 
-'''generate n random data'''
-def generate_data(n):
-    cmd = "INSERT INTO activity_history(username, nama_aktivitas, kategori, jumlah_bensin, total_watt, timestamp_key)"
-    for i in range(n):
+def generate_data(count):
+    '''generate n random data'''
+    cmd = "INSERT INTO activity_history"
+    cmd += "(username, nama_aktivitas, kategori, jumlah_bensin, total_watt, timestamp_key)"
+    for i in range(count):
         waktu = generate_timestamp()
         value = random.randrange(1, 100)
         if i % 2 == 0:
@@ -66,21 +69,23 @@ def generate_data(n):
         mycursor.execute(cmd)
         mydb.commit()
 
-'''add new activity data (one by one)'''
 def add_activity(data):
-    cmd = "INSERT INTO activity_history(username, nama_aktivitas, kategori, jumlah_bensin, total_watt, timestamp_key)"
+    '''add new activity data (one by one)'''
+    cmd = "INSERT INTO activity_history"
+    cmd += "(username, nama_aktivitas, kategori, jumlah_bensin, total_watt, timestamp_key)"
     cmd += f" VALUES (\"{data[0]}\", \"{data[1]}\", \"{str(data[2])}\", \"{data[3]}\", \"{data[4]}\", \"{data[5]}\");"
     mycursor.execute(cmd)
     mydb.commit()
 
-'''commit the changes in a data'''
 def edit_activity(data):
+    '''commit the changes in a data'''
     data2 = (data[2], data[3], data[4], data[5], data[6], data[0])
     cmd = f"UPDATE activity_history SET nama_aktivitas = \"{data2[0]}\", kategori = \"{data[1]}\", jumlah_bensin = \"{data[2]}\", total_watt = \"{data[3]}\", timestamp_key = \"{data[4]}\" WHERE username = \"{current_username}\" AND activityid = \"{data[5]}\""
     mycursor.execute(cmd)
     mydb.commit()
 
 def delete_activity(data):
+    '''delete a data based on the id and username'''
     cmd = f"DELETE FROM activity_history WHERE username = \"{current_username}\"  AND activityid = \"{data[0]}\""
     mycursor.execute(cmd)
     mydb.commit()
@@ -107,8 +112,8 @@ def get_older_data(data_length):
         data.append(i)
     return data
 
-'''return a tuple of data based on the id'''
 def get_data_from_id(activityid):
+    '''return a tuple of data based on the id'''
     cmd = f"SELECT * FROM activity_history WHERE username = \"{current_username}\" AND activityid = {activityid}"
     mycursor.execute(cmd)
     return mycursor.fetchone()
@@ -122,27 +127,42 @@ def get_all_tips_and_tricks():
         data.append(i)
     return data
 
-'''
-1 jumlah_bensin = 2.232 kg CO2 emission
-1 total_watt = 0.233 kg CO2 emission
-'''
+
+#1 jumlah_bensin = 2.232 kg CO2 emission
+#1 total_watt = 0.233 kg CO2 emission
+
 def get_total_emission():
+    ''' return total emission '''
     cmd = f"SELECT SUM(jumlah_bensin), SUM(total_watt) FROM activity_history WHERE username = \"{current_username}\""
     mycursor.execute(cmd)
     return mycursor[0]*2.232 + mycursor[1]*0.233
 
-def get_previous_day_emission(n):
-    cmd = f"SELECT SUM(jumlah_bensin), SUM(total_watt) FROM activity_history WHERE username = \"{current_username}\" AND DATEDIFF(NOW(), timestamp_key) <= {n}"
+def get_previous_day_emission(count):
+    ''' return total emission for count days '''
+    cmd = "SELECT SUM(jumlah_bensin), SUM(total_watt) FROM activity_history "
+    cmd += f"WHERE username = \"{current_username}\" AND DATEDIFF(NOW(), timestamp_key) <= {count}"
     mycursor.execute(cmd)
     return mycursor[0]*2.232 + mycursor[1]*0.233
 
-def get_previous_week_emission(n):
-    return get_previous_day_emission(n*7)
+def get_previous_week_emission(count):
+    ''' return total emission for count weeks '''
+    return get_previous_day_emission(count*7)
 
-def get_previous_month_emission(n):
-    return get_previous_day_emission(n*30)
+def get_previous_month_emission(count):
+    ''' return total emission for count months '''
+    return get_previous_day_emission(count*30)
 
-def get_previous_day_emission(n):
-    cmd = f"SELECT SUM(jumlah_bensin), SUM(total_watt) FROM activity_history WHERE username = \"{current_username}\" AND DATEDIFF(NOW(), timestamp_key) <= {n}"
+def get_previous_day_sum(count):
+    ''' return total sum for count days '''
+    cmd = "SELECT SUM(jumlah_bensin), SUM(total_watt) FROM activity_history "
+    cmd += f"WHERE username = \"{current_username}\" AND DATEDIFF(NOW(), timestamp_key) <= {count}"
     mycursor.execute(cmd)
     return mycursor.fetchone()
+
+def get_previous_week_sum(count):
+    ''' return total sum for count weeks '''
+    return get_previous_day_sum(count*7)
+
+def get_previous_month_sum(count):
+    ''' return total sum for count months '''
+    return get_previous_day_sum(count*30)
